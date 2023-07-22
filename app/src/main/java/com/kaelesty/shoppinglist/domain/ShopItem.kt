d41f957e0a22b5a93290373b5a06e9970724b959
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-07-22 17:31:30 +0300

    Data layer Simple var-based data containment

--- app/src/main/java/com/kaelesty/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/kaelesty/shoppinglist/domain/ShopItem.kt
@@ -1,8 +1,10 @@
 package com.kaelesty.shoppinglist.domain
 
+const val SHOP_ITEM_EMPTY_ID = -1
+
 data class ShopItem(
-    val id: Int,
     val name: String,
     val quanity: Int,
-    val isActive: Boolean
+    val isActive: Boolean,
+    var id: Int = SHOP_ITEM_EMPTY_ID,
 )
