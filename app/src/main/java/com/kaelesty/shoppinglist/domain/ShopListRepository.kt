4754d9fe687842c01ab029c76667ac9aa32b467b
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-07-23 14:36:20 +0300

    Presentation layer ViewModel

--- app/src/main/java/com/kaelesty/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/kaelesty/shoppinglist/domain/ShopListRepository.kt
@@ -1,10 +1,12 @@
 package com.kaelesty.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
 
     fun addShopItem(shopItem: ShopItem)
 
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
     fun getShopItemById(id: Int): ShopItem
 
--- app/src/main/java/com/kaelesty/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/kaelesty/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kaelesty.shoppinglist
+package com.kaelesty.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kaelesty.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
